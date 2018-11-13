3e3186136e94582b12fd9f5d7f726e38d2789779
Daniel Dormer <daniel.dormer@mymona.uwi.edu>
2018-11-13 07:23:56 +0000

    final

--- request.php
+++ request.php
@@ -1,18 +1,53 @@
 <?php
 
-// accept a term (keyword)
-// respond with a value
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<entries>
+     <definition>
+        <name> Definition </name>
+        <author> John </author>
+        <words>A statement of the exact meaning of a word, especially in a dictionary.</words>
+    </definition>
+    
+    <definition>
+        <name> Bar </name>
+        <author> Mary </author>
+        <words>A place that sells alcholic beverages.</words>
+    </definition>
+    
+    <definition >
+        <name> AJAX </name>
+        <author> Kimberley </author>
+        <words>Technique which involves the use of javascript and xml (or JSON).</words>
+    </definition>
+    
+    <definition>
+        <name> HTML </name>
+        <author> Thresh </author>
+        <words>The standard markup language for creating web pages and web applications.</words>
+    </definition>
+    
+    <definition>
+        <name> CSS </name>
+        <author> Pob </author>
+        <words>A style sheet language used for describing the presentation of a document written in a markup language.</words>
+    </definition>
+    
+    <definition>
+        <name> JavaScript </name>
+        <author> Karl </author>
+        <words>A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.</words>
+    </definition>
+    
+    <definition>
+        <name> PHP </name>
+        <author> Lion </author>
+        <words> A server-side scripting language, and a powerful tool for making dynamic and interactive websites.</words>
+    </definition>
+    
+</entries>'; 
 
-$query = $_GET['q'];
-$definition = [
-    "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
-    "bar" => "A place that sells alcholic beverages",
-    "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
-    "html" => "The standard markup language for creating web pages and web applications.",
-    "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
-    "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
-    "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
-];
+header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+echo $xmlOutput->asXML();
 
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+
